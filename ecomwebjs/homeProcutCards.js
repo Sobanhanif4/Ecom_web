f2fff918370a3f2542744567b0b60377490fe154
Soban hanif <sobanatban4@gmail.com>
2024-07-17 14:35:44 +0500

    added add to cart

--- ecomwebjs/homeProcutCards.js
+++ ecomwebjs/homeProcutCards.js
@@ -1,3 +1,6 @@
+import { addToCart } from "./addToCart";
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate");
 
@@ -13,6 +16,8 @@ export const showProductContainer = (products) => {
 
         const productClone = document.importNode(productTemplate.content, true);
 
+        productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
+
         productClone.querySelector(".productName").textContent = name;
         productClone.querySelector(".category").textContent = category;
         productClone.querySelector(".productImage").src = image;
@@ -21,7 +26,16 @@ export const showProductContainer = (products) => {
         productClone.querySelector(".productDescription").textContent = description;
         productClone.querySelector(".productPrice").textContent = `PKR ${price}`;
         productClone.querySelector(".productActualPrice").textContent = `PKR ${price * 4
-        }`;
+            }`;
+
+
+        productClone.querySelector(".stockElement").addEventListener("click", (event) => {
+            homeQuantityToggle(event, id, stock);
+        });
+
+        productClone.querySelector(".add-to-cart-button").addEventListener("click",(event) => {
+            addToCart(event, id, stock);
+        });
 
         productContainer.append(productClone);
 
